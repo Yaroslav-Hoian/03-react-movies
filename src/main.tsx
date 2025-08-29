80cdf2cb38b849b673a76600f5c6e17c4b9d04a8
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-08-29 14:01:45 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "modern-normalize";
+import "./global.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
